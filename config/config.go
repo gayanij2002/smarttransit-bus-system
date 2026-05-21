2ca93b7f6920da0780e2a64b5d35efd9ca1bceb0
gayanij <gayanij2002@gmail.com>
2026-05-21 17:56:09 +0530

    fix root config

--- config/config.go
+++ config/config.go
@@ -2,15 +2,21 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
 
+	if os.Getenv("CHOREO") != "" {
+		log.Println("Running on Choreo")
+		return
+	}
+
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("No .env file found")
 	}
 }
